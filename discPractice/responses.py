cfae292c3092fbec1280a79a4fc9f652fc5df40e
Berat <140023449+bercev@users.noreply.github.com>
2024-08-25 22:52:57 -0700

    Update responses.py

--- discPractice/responses.py
+++ discPractice/responses.py
@@ -1,10 +1,19 @@
 from random import choice, randint
-from aiTest import ask_chat
+from aiTest import ask_chatAI, askQnA
+from collections import defaultdict
 
 # responses based on input
-def get_response(user_input: str, deleted: bool=False) -> str:
+def get_response(user_input: str=None, deleted: bool=False, allMessages: defaultdict=None) -> str:
     lowered: str = user_input.lower()
 
+    if '>>getresult' in lowered[0:11] and allMessages:
+        l =  []
+        for k, v in allMessages.items():
+            l.extend(v)
+        allMsgString = ' '.join(l)
+        print("[SYSTEM] asking the model...")
+        return askQnA({"inputs": {"question": lowered[11:],"context": allMsgString}})
+        
     if deleted:
         return lowered
     if lowered =='':
@@ -14,9 +23,12 @@ def get_response(user_input: str, deleted: bool=False) -> str:
     if 'how are you' in lowered:
         return 'Good, thanks!'
     if 'bye' in lowered:
-        return 'cya'
+        return 'Bye!'
     if 'roll dice' in lowered:
         return f'You rolled: {randint(1,6)}'
     if len(lowered) > 2 and '>>' in lowered[0:2] and '?' in lowered[2:]: # checks if syntax is correct to call the AI model
-        return ask_chat(lowered)
-    return choice(['Take it to a publisher bro', 'Ion know what u mean', 'Idk how to answer dat lfmao','code not implemented for yo bs yet'])
+        if '[500]' in lowered:
+            return "500 word response:\n" + ask_chatAI(lowered)[0:500]
+        return ask_chatAI(lowered)[0:2000]
+    
+    return #choice(['Take it to a publisher bro', 'Ion know what u mean', 'Idk how to answer dat lfmao','code not implemented for yo bs yet'])
