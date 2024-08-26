bb55f427a1bf86573c94b4d24ce10e5b6c8ca676
Berat <140023449+bercev@users.noreply.github.com>
2024-08-25 20:24:49 -0700

    Update responses.py
    updated responses

--- discPractice/responses.py
+++ discPractice/responses.py
@@ -1,8 +1,12 @@
 from random import choice, randint
+from aiTest import ask_chat
 
-def get_response(user_input: str) -> str:
+# responses based on input
+def get_response(user_input: str, deleted: bool=False) -> str:
     lowered: str = user_input.lower()
 
+    if deleted:
+        return lowered
     if lowered =='':
         return 'Well, ur silent one aren\'t ya!'
     if 'hello' in lowered:
@@ -13,5 +17,6 @@ def get_response(user_input: str) -> str:
         return 'cya'
     if 'roll dice' in lowered:
         return f'You rolled: {randint(1,6)}'
-    return choice(['Take it to a publisher bro', 'Ion know what u mean', 'Idk how to answer dat lfmao',
-                   'code not implemented for yo bs yet'])
+    if len(lowered) > 2 and '>>' in lowered[0:2] and '?' in lowered[2:]: # checks if syntax is correct to call the AI model
+        return ask_chat(lowered)
+    return choice(['Take it to a publisher bro', 'Ion know what u mean', 'Idk how to answer dat lfmao','code not implemented for yo bs yet'])
