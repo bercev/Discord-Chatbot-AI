b8271a587da83db186d7b9a07521377da29451c4
Berat <140023449+bercev@users.noreply.github.com>
2024-08-25 22:52:44 -0700

    Update main.py
    added question and answer from context functionality using local storage

--- discPractice/main.py
+++ discPractice/main.py
@@ -5,6 +5,7 @@ from discord import Intents, Client, Message
 from responses import get_response
 import certifi
 import asyncio
+from collections import defaultdict
 
 os.environ["SSL_CERT_FILE"] = certifi.where()
 
@@ -17,6 +18,12 @@ TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 intents: Intents = Intents.default()
 intents.message_content = True # NOQA
 client: Client = Client(intents=intents)
+
+# deleted message holder
+recentlyDeleted = "No messages have been recently deleted"
+
+# holds all messages
+allMessages = defaultdict(set)
 
 
 # Flag to control message processing
@@ -33,36 +40,30 @@ async def send_message(message: Message, user_message: str, deleted: bool=False)
     if message.author == client.user:
         return
     try:
-        if deleted:
-            response: str = get_response(user_message, deleted=True)
+        # this if block checks if a message that has been deleted wants to be viewed
+        if '>>snipe' in user_message.lower():
+            response: str = recentlyDeleted # new version of deleted only works when '>>Snipe' is mentioned
+            # {old version} --> response: str = get_response(user_message, deleted=True)
+        elif '>>getresult' in user_message.lower():
+            response: str = get_response(user_message, deleted=False, allMessages=allMessages)
         else:
             response: str = get_response(user_message)
         await message.author.send(response) if is_private else await message.channel.send(response)
         
-        # if a question is asked to the chatbot, sleep for 5 seconds to ensure the AI model isn't overloaded because of server requests 
-        # as it is hosted on the Hugging Facec Inference API
+        ''' if a question is asked to the chatbot, sleep for 5 seconds to ensure the AI model isn't overloaded because of server requests 
+        as it is hosted on the Hugging Face Inference API
     
-        # if '>>' in user_message:
-            # processing_messages = False
-            # sleepTime: str = "Not processing any messages for the next 5 seconds..."
-            # await message.author.send(sleepTime) if is_private else await message.channel.send(sleepTime)
-            # await asyncio.sleep(5)
-            # processing_messages = True
-            # await message.author.send("Im back!") if is_private else await message.channel.send("Im back!")
+        if '>>' in user_message:
+            processing_messages = False
+            sleepTime: str = "Not processing any messages for the next 5 seconds..."
+            await message.author.send(sleepTime) if is_private else await message.channel.send(sleepTime)
+            await asyncio.sleep(5)
+            processing_messages = True
+            await message.author.send("Im back!") if is_private else await message.channel.send("Im back!")
+            '''
     except Exception as e:
         print(e)
 
-
-# Handling the startup for our bot
-@client.event
-async def on_read(message: Message) ->None:
-    print(f'{client.user} is now running!')
-    await send_message(message, "Bot is now live!")
-
-# Status update of the bot when it first goes live
-@client.event
-async def on_ready():
-    print("BOT IS NOW LIVE!")
 
 # Handling incoming messages
 @client.event
@@ -74,8 +75,17 @@ async def on_message(message: Message) -> None:
     user_message: str = message.content
     channel: str = str(message.channel)
 
+    # adding the message to number of messages read
+    addToAllMsgDictionary(username, user_message, channel)
+
     print(f'[{channel}] {username}: {user_message}')
     await send_message(message, user_message)
+
+# adds message to the dictionary
+def addToAllMsgDictionary(username: str, user_message: str, channel:str) -> None:
+        global allMessages
+        allMessages[username].add(f"||{username} in channel #{channel} sent this message: {user_message}||")
+
 
 # Detects deleted messages
 @client.event
@@ -87,10 +97,26 @@ async def on_message_delete(message: Message) -> None:
     user_message: str = message.content
     channel: str = str(message.channel)
 
-    user_message = f'<-DELETED->[{channel}] {username}: {user_message}'
+    user_message = f'**{message.author.mention} in #{channel} recently deleted:** {user_message}'
     print(user_message)
 
-    await send_message(message, user_message, deleted=True)
+    # await send_message(message, user_message, deleted=True) <-- Old version; always shows deleted message
+    global recentlyDeleted 
+    recentlyDeleted = user_message
+
+
+# Handling the startup for our bot
+@client.event
+async def on_read(message: Message) ->None:
+    print(f'{client.user} is now running!')
+    await send_message(message, "Bot is now live!")
+
+
+# Status update of the bot when it first goes live
+@client.event
+async def on_ready():
+    print("BOT IS NOW LIVE!")
+
 
 # Main entry point
 def main() -> None:
